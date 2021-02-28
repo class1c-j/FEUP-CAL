f2f975a5298f7488921d40188ad35eeb99eceb23
eukia <ritam.mendes@gmail.com>
2021-02-28 17:54:57 +0000

    Solve tp1

--- TP1/ex5.cpp
+++ TP1/ex5.cpp
@@ -1,34 +1,27 @@
 #include "exercises.h"
 
 bool isCanonical(unsigned int C[], unsigned int n) {
-    //TODO
     unsigned int stock[n];
     unsigned int min = C[2] + 2;
     unsigned int max = C[n-2] + C[n-1];
 
     // the counter-example will always be in this interval
     for (unsigned int t = min; t < max; ++ t) {
-
         for (unsigned j = 0; j < n; ++j) {
             stock[j] = n;
         }
-
         unsigned int bfSolution[n]; unsigned int bfSum = 0;
         changeMakingBF(C, stock, n, t, bfSolution);
         for (unsigned int i = 0; i < n; ++i) {
             bfSum += bfSolution[i];
         }
-
         unsigned int greedySolution[n]; unsigned int greedySum = 0;
         changeMakingGreedy(C, stock, n, t, greedySolution);
         for (unsigned int i = 0; i < n; ++i) {
             greedySum += greedySolution[i];
         }
-
         if (greedySum != bfSum) return false;
-
     }
-
     return true;
 }
 
