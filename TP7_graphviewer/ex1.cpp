e0b79383358dbfe890f3bc95663bdb702d9a23cc
eukia <ritam.mendes@gmail.com>
2021-04-27 14:38:01 +0100

    Start graphviewer

---
+++ TP7_graphviewer/ex1.cpp
@@ -0,0 +1,43 @@
+#include "graphviewer.h"
+
+using namespace std;
+using Node = GraphViewer::Node;
+using Edge = GraphViewer::Edge;
+
+void ex1() {
+
+    // Instantiate GraphViewer
+    GraphViewer gv;
+    // Set coordinates of window center
+    gv.setCenter(sf::Vector2f(300, 300));
+
+
+    Node &node0 = gv.addNode(0, sf::Vector2f(200, 300)); // Create node
+    node0.setColor(GraphViewer::BLUE); // Change color
+
+    Node &node1 = gv.addNode(1, sf::Vector2f(400, 300));
+    node1.setColor(GraphViewer::BLUE);
+
+    Edge &edge1 = gv.addEdge(0, node0, node1);
+
+    gv.removeNode(1);
+
+    Node &node2 = gv.addNode(2, sf::Vector2f(500, 300));
+    Edge &edge2 = gv.addEdge(1, node0, node2);
+
+    node2.setLabel("This is a vertex");
+    edge2.setLabel("This is an edge");
+
+    node2.setColor(GraphViewer::GREEN);
+
+    for (Edge *edge : gv.getEdges()) {
+        edge->setColor(GraphViewer::YELLOW);
+        gv.setBackground("../TP7_graphviewer/resources/background.png");
+    }
+
+    // Create window
+    gv.createWindow(600, 600);
+    // Join viewer thread (blocks till window closed)
+    gv.join();
+
+}
